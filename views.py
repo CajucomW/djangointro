c89e03c9c97062b7355850ed0e25a4b19d7b0f6b
Wence Cajucom <cajucom.w@gmail.com>
2019-07-09 23:56:44 -0700

    Modified views.py to simplify views.py.

--- views.py
+++ views.py
@@ -4,78 +4,21 @@ from jinja2 import Template
 from django.shortcuts import render
 
 def index(request):
+    print("---Processing Index page---")
     context = {}
     return render(request, 'index.html', context)
 
 def tech(request):
+    print("---Processing Tech page---")
     context = {}
     return render(request, 'tech.html', context)
 
 def pta(request):
+    print("---Processing PTA page---")
     context = {}
     return render(request, 'pta.html', context)
     
 def jiujitsu(request):
+    print("---Processing Jiujitsu page---")
     context = {}
     return render(request, 'jiujitsu.html', context)
-
-# def templates(content, template, html_file):
-#     combined = template.replace('{{content}}', html_file)
-#     combined = combined.replace('{{title}}', content['title'])
-#     combined = combined.replace(content['active'], 'active')
-#     return combined
-
-# def process(content):
-#     template = open('templates/base.html').read()
-#     html_file = open(content['filename']).read()
-#     combined = templates(content, template, html_file)
-#     open(content['output'], 'w+').write(combined)
-
-# def main():
-#     for content in pages:
-#         print("Processing...", content['title'])
-#         process(content)
-#     print('Done')
-
-# if __name__ == "__main__":
-#     main()   
-
-# def html_files():
-#     pages = []
-    
-#     all_html_files = glob.glob("content/*.html")
-#     output = glob.glob('docs/*.html')
-
-#     for html_path in all_html_files:                            # html_path is content/*.html
-#         file_name = os.path.basename(html_path)                 # file_name is *.html
-#         name_only, extension = os.path.splitext(file_name)      # name_only is *
-#         html_content = open(html_path).read()
-#         doc_dir = "docs/" + file_name
-
-#         pages.append({
-#             "title": name_only,
-#             "output": doc_dir,
-#             "filename": file_name,
-#             "content": html_content,
-#         })
-#     return render(request, 'html_files', context)
-
-# def template_pages():
-#     template_html = open('templates/base.html').read()
-
-#     for page in pages:
-#         title = page['title']
-#         output = page['output']
-#         filename = page['filename']                             # filename is content/*html from list
-#         content = page['content']
-#         template = Template(template_html)
-#         result = template.render(
-#             title=title, 
-#             output=output,
-#             filename=filename,
-#             content=content,
-#             pages=pages,       #### <<< links break when this line isn't inlcuded. Not sure why. ####
-#             )
-#         open(page['output'], 'w+').write(result)
-#     return render(request, 'template_pages', context)
-
