c7e9c548f8264f30d3be8de02bbe41d09e0c45eb
Wence Cajucom <cajucom.w@gmail.com>
2019-07-13 09:51:29 -0700

    Minor edits to html files prior to submission.

--- urls.py
+++ urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 
 import views
 
-# # In this example, we've separated out the views.py into a new file
 urlpatterns = [
     path('', views.index, name="Wence Cajucom"),
     path('tech', views.tech, name="Tech"),
@@ -10,6 +9,20 @@ urlpatterns = [
     path('pta', views.pta, name="PTA"),
     path('jiujitsu', views.jiujitsu, name="Jiujitsu"),
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Boilerplate to include static files
 from django.conf import settings
